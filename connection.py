7307ae478c94e7c0356c9f8c9975b918aaf17cd8
Sokil Natalia <sokilnatalka@gmail.com>
2023-03-29 00:02:48 +0300

    add env

--- connection.py
+++ connection.py
@@ -1,13 +1,16 @@
 from psycopg2 import connect, Error
 from contextlib import contextmanager
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 @contextmanager
 def create_connection():
     """ create a database connection to a Postgres database """
     conn = None
     try:
-        conn = connect(host='hattie.db.elephantsql.com', user='gvyehsbp', password='X1iuWunDD1jMgcASxl_iI9E1p-nKBTzl', database='gvyehsbp', port=5432)
+        conn = connect(host=os.getenv("HOST"), user=os.getenv("USER"), password=os.getenv("USER"), database=os.getenv("DATABASE"), port=5432)
         yield conn
         conn.rollback()
         conn.close()
